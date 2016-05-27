53a292381adc879e83f590325934b6933af00bd2
Ishita Chourasia <ishitach@users.noreply.github.com>
2016-05-27 16:32:11 +0530

    Add files via upload

---
+++ ll_intersection_1.java
@@ -0,0 +1,46 @@
+/* package whatever; // don't place package name! */
+
+/* Name of the class has to be "Main" only if the class is public. */
+class LinkedList
+{ static Node head;
+ static class Node{
+	int data;
+	Node next;
+		Node(int d) {
+			data=d;next=null;
+	}
+	}
+	
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+	//	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	LinkedList ll =new	LinkedList();
+	ll.head =new Node(3);
+	Node n2=new Node(6);
+	Node n3 =new Node(9);
+	Node n4=new Node(15);
+	Node n6=new Node(30);
+	Node n5=new Node(10);
+	ll.head.next=n2;n2.next=n3;n3.next=n4;n4.next=n6;
+	n5.next=n4;n5.next.next=n6;
+	// Node n=ll.head;
+	// while(n!=null){
+	// 	System.out.println(n.data);
+	// n=n.next;
+	// }
+	Node i=ll.head;
+	p:{while(i!=null){
+		Node j=n5;
+		while(j!=null){
+			if(i.data==j.data){
+				System.out.println(i.data);
+				break p;
+			}
+			j=j.next;
+		}i=i.next;
+	}
+	
+	}
+	}
+}
