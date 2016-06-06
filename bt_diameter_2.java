e40c88fab7d3ab5a5cf3747175df6843943731b3
Ishita Chourasia <ishitach@users.noreply.github.com>
2016-06-06 21:57:16 +0530

    Add files via upload

---
+++ bt_diameter_2.java
@@ -0,0 +1,67 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class BinaryTree
+{static Node root;
+	static class Node
+{
+    int key;
+    Node left, right;
+ 
+    public Node(int item)
+    {
+        key = item;
+        left = right = null;
+    }
+}
+static class Height{
+	int h;
+}
+BinaryTree(int key){
+	root=new Node(key);
+}
+BinaryTree(){
+	root=null;
+}
+
+	
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		
+		BinaryTree bt=new BinaryTree();
+		bt.root=new Node(1);
+		bt.root.left=new Node(2);
+		bt.root.right=new Node(3);
+		bt.root.left.left=new Node(4);
+		bt.root.left.right=new Node(5);
+		Height height=new Height();
+		int c=bt.diameter(root,height);
+		System.out.println(c);
+	}
+	public static int height(Node node){
+		if(node==null) return 0;
+		
+		return (1+Math.max(height(node.right),height(node.left)));
+	}
+	
+	 public static int diameter(Node root, Height height){
+	 	
+	 	Height lh=new Height();Height rh=new Height();
+	 	
+	 	if(root==null) 
+	 	{height.h=0;
+	 	return 0;}
+	 	lh.h++;rh.h++;
+	 	
+	 	int ld=diameter(root.left,lh);
+	 	int rd=diameter(root.right,rh);
+	 	height.h=1+Math.max(rh.h,lh.h);
+	 	return Math.max(Math.max((ld),(rd)),1+lh.h+rh.h);
+	 	
+	 }
+}
